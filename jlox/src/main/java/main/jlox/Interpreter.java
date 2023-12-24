fbaedc88bd59524b4a13a25984f36931fcc65f8c
bh34e5 <b.hester0614@yahoo.com>
2023-12-24 17:58:11 -0500

    part of the interpreter implementation

---
+++ jlox/src/main/java/main/jlox/Interpreter.java
@@ -0,0 +1,74 @@
+package main.jlox;
+
+public class Interpreter implements Expr.Visitor<Object> {
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case MINUS:
+                return (double) left - (double) right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+                break;
+            case SLASH:
+                return (double) left / (double) right;
+            case STAR:
+                return (double) left * (double) right;
+
+            default:
+                break;
+
+        }
+
+        // unreachable code
+        return null;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                return -(double) right;
+
+            default:
+                break;
+        }
+
+        // unreachable code
+        return null;
+    }
+
+    private boolean isTruthy(Object value) {
+        return !isFalsey(value);
+    }
+
+    private boolean isFalsey(Object value) {
+        return value == null || Boolean.FALSE.equals(value);
+    }
+}
